7c26dee1e475591e070effa438fb2bb93c3ba1ce
Geelani Haynes <geelani245@hotmail.com>
2025-09-12 01:51:09 -0400

    Fresh start
    Started over. had difficulties with getting it to run dev locally. Only express , dotenv, cors, and prisma installed as dependencies for now.  database is hosted locally on docker and is connected to backend via prisma.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,14 @@
-// import express
-const express = require("express");
-
-
-
-const { PrismaClient } = require("./generated/prisma_client");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+const PORT = 8000;
 
-const prisma = new PrismaClient();
-
-app.use(express.json());
-
-
-
-const PORT = 3000;
+app.get("/", (_req, res) => {
+  res.send("Hello from Express 🚀");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
