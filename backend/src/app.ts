c213585bb8cd91e4b2ece37367dd96b18397da9d
Geelani Haynes <geelani245@hotmail.com>
2025-09-23 22:27:13 -0400

    Backend connected to front end
    Backend is connected to frontend for local development. Local IP needs to be changed manually for now.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import usersRouter from "./routes/users.routes";
 
+//import cors to enable cross-site origin requests outside of basic get post
+import cors from "cors";
+
+import dotenv from "dotenv";
+
 const app = express();
+
+// Add Cors to express for use
+app.use(cors());
 
 // Middleware
 app.use(express.json());
 
 // Health check
-app.get("/", (_req, res) => {
-  res.status(200).send("Hello from Express 🚀");
+app.get("/api", (_req, res) => {
+  res.json({ status: "ok"});
 });
 
 // Mount user routes
